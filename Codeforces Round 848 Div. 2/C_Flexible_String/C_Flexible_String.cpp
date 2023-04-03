2ca67ec8c56ef61ace6d6278a80a282d1eae6364
caiprozect <caiprozect@gmail.com>
2023-04-03 17:58:19 +0900

    Understand a solution for 1778D

--- Codeforces Round 848 Div. 2/C_Flexible_String/C_Flexible_String.cpp
+++ Codeforces Round 848 Div. 2/C_Flexible_String/C_Flexible_String.cpp
@@ -2,7 +2,7 @@
 Description
 You have a string a and a stirng b. Both of the strings have length n.
 There are at most 10 different characters in the string a.
-You also have a set Q. Initially, Q is empty.  
+You also have a set Q. Initially, Q is empty.
 You can apply the following operation on the string a any number of times:
     Choose an index 1 <= i <= n and a lowercase English letter c.
     Add a_i to the set Q and then replace a_i with c.
@@ -12,8 +12,8 @@ Under this constraint, you have to maximize the number of integer pairs (l, r)
 (1 <= l <= r <= n) such that a[l, r] = b[l, r].
 
 Input
-1 <= t <= 1e4  
-1 <= n <= 1e5, 0 <= k <= 10  
+1 <= t <= 1e4
+1 <= n <= 1e5, 0 <= k <= 10
 a of length n
 b of length n
 
@@ -26,58 +26,74 @@ The maximum number of pairs (l, r).
 using namespace std;
 
 #define ll long long
-#define pb push_back 
-#define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define pb push_back
+#define fastio                             \
+    std::ios_base::sync_with_stdio(false); \
+    cin.tie(0);                            \
+    cout.tie(0);
 
-ll count_matching_pair() {
+string a, b;
+string char_list;
+bool mark[26];
+ll ans, k;
+
+ll count_matching_pair()
+{
     ll tot_pair = 0, match_count = 0;
 
-    for (ll i = 0; i < a.size(); i++) {
-        if (a[i] == b[i] || mark[ a[i]-'a' ]) match_count++;
-        else {
-            tot_pair += match_count*(match_count+1)/2;
+    for (ll i = 0; i < a.size(); i++)
+    {
+        if (a[i] == b[i] || mark[a[i] - 'a'])
+            match_count++;
+        else
+        {
+            tot_pair += match_count * (match_count + 1) / 2;
             match_count = 0;
         }
     }
-    tot_pair += match_count*(match_count+1)/2;
+    tot_pair += match_count * (match_count + 1) / 2;
 
     return tot_pair;
 }
 
-void solve(ll pos, ll cnt) {
-    if (cnt > k) return;
+void solve(ll pos, ll cnt)
+{
+    if (cnt > k)
+        return;
 
-    if (pos == char_list.size()) {
-        if (cnt == k) ans = max(anx, count_matching_pair());
+    if (pos == char_list.size())
+    {
+        if (cnt == k)
+            ans = max(ans, count_matching_pair());
         return;
     }
 
-    solve(pos+1, cnt);
+    solve(pos + 1, cnt);
 
-    mark[ char_list[pos]-'a' ] = 1;
-    solve(pos+1, cnt+1);
-    mark[ char_list[pos]-'a' ] = 0;
+    mark[char_list[pos] - 'a'] = 1;
+    solve(pos + 1, cnt + 1);
+    mark[char_list[pos] - 'a'] = 0;
 }
 
-int main() {
+int main()
+{
     fastio;
     ll t;
     cin >> t;
-    
-    string a, b;
-    string char_list;
-    bool mark[26];
-    ll ans, k;
 
-    while (t--) {
-        ll n; cin >> n >> k;
+    while (t--)
+    {
+        ll n;
+        cin >> n >> k;
         cin >> a >> b;
 
         unordered_set<char> unq;
-        for(auto &ch: a) unq.insert(ch);
+        for (auto &ch : a)
+            unq.insert(ch);
 
         char_list.clear();
-        for(auto &x: unq) char_list.pb(x);
+        for (auto &x : unq)
+            char_list.pb(x);
 
         k = min(k, (ll)unq.size());
         memset(mark, 0, sizeof mark);
---
+++ Codeforces Round 848 Div. 2/D_Flexible_String_Revisit/D_Flexible_String_Revisit.cpp
@@ -0,0 +1,18 @@
+/*
+Description
+You are given two binary strings a and b of length n.
+In each move, the string a is modified in the following way:
+    An index 1 <= i <= n is chosen uniformly at random.
+    The character a_i will be flipped.
+What is the expected number of moves required to make
+both strings equal for the first time?
+
+Input
+1 <= t <= 1e5
+1 <= n <= 1e6
+a
+b
+
+Output
+The expected number modulo 998 244 353
+*/
