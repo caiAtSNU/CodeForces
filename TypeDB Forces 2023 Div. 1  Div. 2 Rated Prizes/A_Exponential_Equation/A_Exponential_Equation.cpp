4a8f6d9975843e412a61d38b38d245d736f70005
caiprozect <caiprozect@gmail.com>
2023-04-04 17:58:37 +0900

    Describe 1787A

---
+++ TypeDB Forces 2023 Div. 1  Div. 2 Rated Prizes/A_Exponential_Equation/A_Exponential_Equation.cpp
@@ -0,0 +1,14 @@
+/*
+Description
+You are given an integer n.
+Find any pair of integers (x, y) (1 <= x,y <= n)
+such that x^y * y + y^x * x = n
+
+Input
+1 <= t <= 1e4
+1 <= n <= 1e9
+
+Output
+If possible print x, y
+else -1
+*/
