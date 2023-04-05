667355bd482b0195f61c97dd27c1821734ee1e09
caiAtSNU <jun0605@snu.ac.kr>
2023-04-05 09:44:58 +0900

    Done 1787A

--- TypeDB Forces 2023 Div. 1  Div. 2 Rated Prizes/A_Exponential_Equation/A_Exponential_Equation.cpp
+++ TypeDB Forces 2023 Div. 1  Div. 2 Rated Prizes/A_Exponential_Equation/A_Exponential_Equation.cpp
@@ -12,3 +12,29 @@ Output
 If possible print x, y
 else -1
 */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    int n; cin >> n;
+
+    if (n%2 == 1) {
+        cout << -1 << "\n";
+    }
+    else {
+        cout << 1 << " " << n/2 << "\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
