9e0b230b7ce7608827c8db0948e3b42fbd85d69a
caiAtSNU <jun0605@snu.ac.kr>
2023-04-06 15:36:27 +0900

    Trying 1787D

--- TypeDB Forces 2023 Div. 1  Div. 2 Rated Prizes/D_Game_on_Axis/D_Game_on_Axis.cpp
+++ TypeDB Forces 2023 Div. 1  Div. 2 Rated Prizes/D_Game_on_Axis/D_Game_on_Axis.cpp
@@ -26,12 +26,56 @@ using namespace std;
 void solve() {
     int n; cin >> n;
     
-    vector<int> a(n);
-    for (int i = 0; i < n; i++) {
+    vector<int> a(n+1);
+    vector<vector<int>> adj_list(n+1);
+    vector<int> stack;
+    int x, y;
+    for (int i = 1; i < n+1; i++) {
         cin >> a[i];
+        x = i;
+        y = i + a[i];
+        if (1 <= y && y <= n) {
+            adj_list[y].push_back(x);
+        }
+        else {
+            stack.push_back(i);
+        }
     }
 
-    
+    int S = 0;
+    vector<int> endgame(n+1);
+    while (stack.size() != 0) {
+        x = stack.back();
+        stack.pop_back();
+        if (endgame[x] == 0) endgame[x] = 1;
+        S += 1;
+        for (int y : adj_list[x]) {
+            if (endgame[y] != 0) {
+                endgame[y] = endgame[x] + 1;
+                stack.push_back(y);
+            }
+        }
+    }
+
+    int ans = 0;
+    vector<int> visited(n+1);
+    int cur = 1;
+    while (1 <= cur && cur <= n) {
+        if (visited[cur] == 1) break;
+
+        visited[cur] = 1;
+
+        ans += n+2;
+        if (endgame[cur] == 1) {
+            ans += S-1;
+        } else {
+            ans += S;
+        }
+
+        cur = cur + a[cur];
+    }
+
+    cout << ans << "\n";
 }
 
 int main() {
